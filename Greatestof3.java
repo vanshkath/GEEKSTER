503e35611ccde4ad9a87a1f021d46eb542e237c7
Vansh Kathuria <66768081+vanshkath@users.noreply.github.com>
2021-10-12 12:03:05 +0530

    Add files via upload

---
+++ Greatestof3.java
@@ -0,0 +1,19 @@
+package java;
+
+public class Greatestof3 {
+    public static void main ( String args[]){
+        int a;
+        int b;
+        int c;
+        if (a>b && a>c){
+            System.out.println("a");
+        }
+        else if (b>a && b>c){
+            System.out.println(b);
+        }
+        else{
+            System.out.println(c);
+        }
+    }
+    
+}
---
+++ Sum.java
@@ -0,0 +1,13 @@
+package java;
+
+public class Sum {
+    public static void main (String args[]){
+        int sum;
+        int i;
+        for (i=0; i<=100; i++){
+            sum = sum + i;
+            System.out.println(sum);
+        }
+    }
+    
+}
